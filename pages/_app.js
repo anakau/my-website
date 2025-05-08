18b680ebbc51ad4dde97531939d5430418b88b50
Anahat Kaur <anahatkaur@Anahats-MacBook-Pro.local>
2025-05-08 09:58:22 +0200

    Add Google Translate widget to _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,51 @@
-import "../styles/globals.css";
+// pages/_app.js
+import { useEffect } from 'react'
+import '../styles/globals.css'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  useEffect(() => {
+    // This function gets called by Google’s script once it loads
+    window.googleTranslateElementInit = () => {
+      /* global google */
+      new google.translate.TranslateElement(
+        {
+          pageLanguage: 'en',
+          layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+        },
+        'google_translate_element'
+      )
+    }
+
+    // Dynamically inject the Google Translate script
+    const script = document.createElement('script')
+    script.src =
+      '//translate.google.com/translate_a/element.js?cb=googleTranslateElementInit'
+    document.body.appendChild(script)
+
+    // Cleanup if the component ever unmounts
+    return () => {
+      delete window.googleTranslateElementInit
+      document.body.removeChild(script)
+    }
+  }, [])
+
+  return (
+    <>
+      {/* Placeholder for the language selector */}
+      <div
+        id="google_translate_element"
+        style={{
+          position: 'fixed',
+          top: 12,
+          right: 12,
+          zIndex: 999,
+          background: 'white',
+          padding: '4px',
+          borderRadius: '4px',
+          boxShadow: '0 1px 4px rgba(0,0,0,0.3)'
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
