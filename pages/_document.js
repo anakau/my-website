eb1394b0284615ea048dc2d33a1ba7fd2027b312
Anahat Kaur <anahatkaur@Anahats-MacBook-Pro.local>
2025-05-08 10:07:25 +0200

    Add custom _document.js to suppress Chrome translate banner

--- pages/_document.js
+++ pages/_document.js
@@ -1,14 +1,17 @@
-// src/pages/_document.js
-import { Html, Head, Main, NextScript } from 'next/document';
+// pages/_document.js
+import { Html, Head, Main, NextScript } from 'next/document'
 
 export default function Document() {
   return (
     <Html>
-      <Head />
+      <Head>
+        {/* Prevent Chrome's own translate banner */}
+        <meta name="google" content="notranslate" />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  );
+  )
 }
